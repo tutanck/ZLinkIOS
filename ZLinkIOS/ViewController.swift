04bb1cdbaa462e7adb0a3a39cb6bdbce2aa3e82b
joan Anagbla <joan@MacBook-Pro-de-joan.local>
2017-08-08 10:02:43 +0200

    Initial Commit

---
+++ ZLinkIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZLinkIOS
+//
+//  Created by joan Anagbla on 08/08/2017.
+//  Copyright © 2017 joan Anagbla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
